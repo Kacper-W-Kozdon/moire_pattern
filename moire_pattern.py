b76dfc2c9332cbd4ff7de645a7d484bc0eb6d7a7
Kacper Kożdoń <102428159+Kacper-W-Kozdon@users.noreply.github.com>
2024-04-14 19:39:38 +0200

    Add files via upload

--- moire_pattern.py
+++ moire_pattern.py
@@ -19,32 +19,31 @@ class Window():
     spin_resolution = math.pi * 8
 
     def __init__(self):
-          self.spin = False
-          self.move = False
-          self.root = None
-          self.canvas = None
-          self.angle = 0
-          self.angle_1 = 0
-          self.angle_2 = 0
-          self.step = 10
-          self.bunch_1 = None
-          self.bunch_2 = None
-          self.main_window()
+        self.spin = False
+        self.move = False
+        self.root = None
+        self.canvas = None
+        self.angle = 0
+        self.angle_1 = 0
+        self.angle_2 = 0
+        self.step = 10
+        self.bunch_1 = None
+        self.bunch_2 = None
+        self.main_window()
 
     def make_it_move(self):
-        if self.move == True:
+        if self.move is True:
             return None
         self.move = True
         if self.spin is not True:
             self.update_canvas()
 
     def make_it_spin(self):
-        if self.spin == True:
+        if self.spin is True:
             return None
         self.spin = True
         if self.move is not True:
             self.update_canvas()
-        
 
     def make_it_stop(self):
         self.move = False
@@ -56,31 +55,33 @@ class Window():
         speed_1, speed_2 = Window.speed_1, Window.speed_2
         num_rays_1, num_rays_2 = Window.num_rays_1, Window.num_rays_2
 
-
         if not all([self.bunch_1, self.bunch_2]):
             return None
 
         if self.spin is True:
 
             self.angle_1 += 1/spin_resolution
-            self.angle_1 = self.angle_1 if self.angle_1 < spin_resolution else 0
+            self.angle_1 = self.angle_1 if \
+                self.angle_1 < spin_resolution else 0
 
             for line_id, line in enumerate(self.bunch_1):
-                self.update_line(line, centre_1, speed_1, self.angle_1, line_id, num_rays_1, direction=1)
+                self.update_line(line, centre_1, speed_1, self.angle_1,
+                                 line_id, num_rays_1, direction=1)
 
             self.angle_2 += 1/spin_resolution
-            self.angle_2 = self.angle_2 if self.angle_2 < spin_resolution else 0
+            self.angle_2 = self.angle_2 if \
+                (self.angle_2 < spin_resolution) else 0
 
             for line_id, line in enumerate(self.bunch_2):
-                self.update_line(line, centre_2, speed_2, self.angle_2, line_id, num_rays_2, direction=-1)
+                self.update_line(line, centre_2, speed_2, self.angle_2,
+                                 line_id, num_rays_2, direction=-1)
 
         if any([self.spin, self.move]):
             self.root.after(20, self.update_canvas)
 
     def update_line(self, line, centre, speed, angle, line_id, num_rays, direction=1):
-        spin_resolution = Window.spin_resolution
         ray_length = Window.ray_length
-        canvas_width, canvas_height = Window.canvas_shape
+        canvas_width = Window.canvas_shape[0]
         centre_x, centre_y = centre
 
         if self.move is True:
@@ -103,10 +104,8 @@ class Window():
             self.canvas.coords(line, x1, y1, x2, y2)
 
     def make_bunch(self, ray_length, centre, num_rays):
-        canvas_width, canvas_height = Window.canvas_shape
         bunch = list()
         self.angle = 0
-        x1, y1, x2, y2 = centre, canvas_height - ray_length, centre, canvas_height + ray_length
 
         for _ in range(num_rays):
             self.angle += 1
@@ -136,7 +135,8 @@ class Window():
         if self.canvas is not None:
             self.canvas.destroy()
 
-        self.canvas = Canvas(self.root, bg="black", width=canvas_width, height=canvas_height)
+        self.canvas = Canvas(self.root, bg="black", width=canvas_width,
+                             height=canvas_height)
 
         self.bunch_1 = self.make_bunch(ray_length, centre_1, num_rays_1)
         self.bunch_2 = self.make_bunch(ray_length, centre_2, num_rays_2)
@@ -155,22 +155,27 @@ class Window():
         exit_button = Button(self.root, text="Exit", command=self.Close)
         exit_button.pack(pady=20)
 
-        move_it_button = Button(self.root, text="Move it", command=self.make_it_move)
+        move_it_button = Button(self.root, text="Move it",
+                                command=self.make_it_move)
         # move_it_button.pack()
 
-        spin_it_button = Button(self.root, text="Spin it", command=self.make_it_spin)
+        spin_it_button = Button(self.root, text="Spin it",
+                                command=self.make_it_spin)
         spin_it_button.pack()
 
-        stop_it_button = Button(self.root, text="Stop it", command=self.make_it_stop)
+        stop_it_button = Button(self.root, text="Stop it",
+                                command=self.make_it_stop)
         stop_it_button.pack()
-        del_shape_button = Button(self.root, text="Restart", 
-                                command= lambda: (self.__setattr__("move", False),
-                                self.__setattr__("spin", False),
-                                self.prep_moire()))
+        del_shape_button = Button(self.root, text="Restart",
+                                  command=lambda:
+                                  (self.__setattr__("move", False),
+                                   self.__setattr__("spin", False),
+                                   self.prep_moire()))
         del_shape_button.pack()
 
         self.prep_moire()
 
         self.root.mainloop()
+
 
 test = Window()
